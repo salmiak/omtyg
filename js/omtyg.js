07b07e5e091e97bd8de34ed50d949efb0837a9ea
Alfred Gunnarsson <alfred@salmiakmedia.se>
2014-05-29 17:29:12 +0200

    Added so that arrow keys navigates carousel on single pages.

--- js/omtyg.js
+++ js/omtyg.js
@@ -3,9 +3,20 @@ $(function(){
   $('#imgCarousel').on('slide.bs.carousel', function (a) {
     // Fade out caption on slide
     $('.capSlider .active').fadeOut().removeClass('active');
-  })
+  });
   $('#imgCarousel').on('slid.bs.carousel', function (a) {
     // Fade in caption on slid
     $('.capSlider [data-caption="' + $('#imgCarousel .active').index('#imgCarousel .item') + '"]').addClass('active').fadeIn(100);
+  });
+  
+  $('body').on('keydown', function(e){
+    if (e.keyCode == 37) { 
+       $('#imgCarousel').carousel('prev');
+       return false;
+    }
+    if (e.keyCode == 39) { 
+       $('#imgCarousel').carousel('next');
+       return false;
+    }
   })
 });
