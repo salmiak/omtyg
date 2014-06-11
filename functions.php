7c622aa3727b6dc91c40028a168e16cf1571c895
Alfred Gunnarsson <alfred@salmiakmedia.se>
2014-06-11 22:49:12 +0200

    Added Google Tracking.

--- functions.php
+++ functions.php
@@ -32,16 +32,16 @@ remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
 //	Lägg till Google Analytics i footern, ändra UA-XXXXX-X till din egen tracking-kod
 //---------------------------------------------------------------------------------
 function add_google_analytics() {	
-	echo '<script type="text/javascript">';
-	echo 'var _gaq = _gaq || [];';
-	echo '_gaq.push(["_setAccount", "UA-XXXXXXX-X"]);';
-	echo '_gaq.push(["_trackPageview"]);';
-	echo '(function() {';
-	echo 'var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;';
-	echo 'ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";';
-	echo 'var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);';
-	echo '})();';
-	echo '</script>';
+	echo "<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51813239-1', 'omtyg.se');
+  ga('send', 'pageview');
+
+</script>";
 }
 add_action('wp_footer', 'add_google_analytics');
 
